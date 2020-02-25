f42020f1e574087b6de424ca999be8f2080e3110
vpec <741294@unizar.es>
2020-02-25 20:17:31 +0100

    some fixes to mongo_test

--- p2ttk/mongo_test.py
+++ p2ttk/mongo_test.py
@@ -5,28 +5,28 @@ el primer y segundo procesado de los textos que se asocian al mismo.
 from sspipe import p
 import re
 # Se importa la clase asociada al etiquetado
-from Etiquetado_parrafos import *
+from p2ttk.par_label import *
 from os import listdir
 from os.path import isfile, join
 
 # Se importan el resto de módulos del programa
-import Extraccion_textos,Preprocesado,Procesado_parrafos
+import p2ttk.txt_ext,p2ttk.pre_proc,p2ttk.par_proc
 
 
 dir_entrada = 'ficheros_entrada'         # Directorio a extraer archivos PDF
 PDF_path_actual  = ''                    # Path del archivo a procesar   
-etiquetado = Etiquetado_parrafos(dir_entrada)
+etiquetado = ParLabel(dir_entrada)
 
 def mostrar_estadisticas():
     '''
         :return:    Muestra por pantalla los 20 mayores párrafos con su respectivo tamaño
     '''
-    Preprocesado.fichero_text('ficheros_salida/conteopalabras.txt','')
-    parrafos = Procesado_parrafos.get_paragraph()
+    p2ttk.pre_proc.fichero_text('ficheros_salida/conteopalabras.txt','')
+    parrafos = p2ttk.par_proc.get_paragraph()
     for numero,parrafo in enumerate(parrafos) :
             if numero > -1:
                 parrafo_proc = "////////////////\nEste parrafo contiene: " + str(Procesado_parrafos.contar_palabras(parrafo)) + " palabras\n"+ parrafo +"\n////////////////\n"       # Muestra el tamaño
-                Preprocesado.fichero_text_append('ficheros_salida/conteopalabras.txt', parrafo_proc)                                            # Muestra el parrafo
+                p2ttk.pre_proc.fichero_text_append('ficheros_salida/conteopalabras.txt', parrafo_proc)                                            # Muestra el parrafo
 
     return True
 
@@ -45,8 +45,8 @@ def segundo_procesado(text):
 
     '''
 
-    texto_procesado = (Procesado_parrafos.extraccion_parrafos(text,etiquetado) | p(Procesado_parrafos.detectar_headers))
-    Preprocesado.fichero_text('ficheros_salida/salida_segundo_procesado.txt',texto_procesado)  
+    texto_procesado = (p2ttk.par_proc.extraccion_parrafos(text,etiquetado) | p(p2ttk.par_proc.detectar_headers))
+    p2ttk.pre_proc.fichero_text('ficheros_salida/salida_segundo_procesado.txt',texto_procesado)  
     return texto_procesado
 
 def primer_procesado(text):
@@ -62,20 +62,20 @@ def primer_procesado(text):
                         "ficheros_entrada"
     '''
   
-    texto_procesado = ( Preprocesado.delete_0C(text)            | p(Preprocesado.delete_false_headers)
-                                                                | p(Preprocesado.etiquetar_listas)
-                                                                | p(Preprocesado.delete_dash)
-                                                                | p(Preprocesado.delete_tabs)
-                                                                | p(Preprocesado.delete_whitespaces)
-                                                                | p(Preprocesado.delete_jumps)
-                                                                | p(Preprocesado.etiquetar_listasOrdenadas)
-                                                                | p(Preprocesado.procesar_listas)
-                                                                | p(Preprocesado.etiquetar_headers)
-                                                                | p(Preprocesado.re_etiquetar_ol)
-                                                                | p(Preprocesado.ajustar_titulos)
+    texto_procesado = ( p2ttk.pre_proc.delete_0C(text)            | p(p2ttk.pre_proc.delete_false_headers)
+                                                                | p(p2ttk.pre_proc.etiquetar_listas)
+                                                                | p(p2ttk.pre_proc.delete_dash)
+                                                                | p(p2ttk.pre_proc.delete_tabs)
+                                                                | p(p2ttk.pre_proc.delete_whitespaces)
+                                                                | p(p2ttk.pre_proc.delete_jumps)
+                                                                | p(p2ttk.pre_proc.etiquetar_listasOrdenadas)
+                                                                | p(p2ttk.pre_proc.procesar_listas)
+                                                                | p(p2ttk.pre_proc.etiquetar_headers)
+                                                                | p(p2ttk.pre_proc.re_etiquetar_ol)
+                                                                | p(p2ttk.pre_proc.ajustar_titulos)
                        )
                                                 
-    Preprocesado.fichero_text_append('ficheros_salida/salida_primer_procesado.txt',texto_procesado)
+    p2ttk.pre_proc.fichero_text_append('ficheros_salida/salida_primer_procesado.txt',texto_procesado)
     return texto_procesado+'\n'  
 
 
@@ -98,7 +98,7 @@ def gestion_procesado():
     '''
 
     lista,archivos = get_listPDF()
-    Preprocesado.fichero_text('ficheros_salida/salida_primer_procesado.txt','')
+    p2ttk.pre_proc.fichero_text('ficheros_salida/salida_primer_procesado.txt','')
     texto_extraido = ''
     texto_total = ''
     i = 0
@@ -108,7 +108,7 @@ def gestion_procesado():
         etiquetado.set_path(archivos[i])
         etiquetado.set_document(archivos[i].split(".")[0])
         
-        texto_extraido = Extraccion_textos.convert_pdf_to_txt(PDF_path_actual)
+        texto_extraido = p2ttk.txt_ext.convert_pdf_to_txt(PDF_path_actual)
         nombre_archivo = archivos[i]
         
         print("Extracción de : "+ archivos[i] + " terminada, iniciando procesado")
