07f94f87202fed1a844321422c8ef9c6ec936764
vpec <victorpenasco1@gmail.com>
2020-05-08 15:03:10 +0200

    auto diff

---
+++ auto_diff.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+string1=".html_pre.md"
+string2=".html_post.md"
+
+file1=""
+file2=""
+
+for file in "$1"/*
+do
+    # Check if file is string1 or string2
+    if [[ "$file" == *"$string1"* ]]
+    then
+        file1="$file"
+    elif [[ "$file" == *"$string2"* ]]
+    then
+        file2="$file"
+    fi
+    # Check if both files have been detected and proceed to diff
+    if [ -n "$file1" ] && [ -n "$file2" ]
+    then
+        echo "$file1"
+        echo "$file2"
+        diff "$file1" "$file2"
+        echo ""
+        # Reset values
+        file1=""
+        file2=""
+    fi
+
+done
+
