4c18df157b7742463324e24dd64a563d919d9797
vpec <victorpenasco1@gmail.com>
2020-05-31 14:30:44 +0200

    docbot

---
+++ docbot/app.py
@@ -0,0 +1,45 @@
+import sys
+
+from os.path import isfile, join, exists, abspath
+from os import listdir, makedirs
+from snips_nlu import SnipsNLUEngine
+from snips_nlu.default_configs import CONFIG_ES
+import json
+import io
+
+seed = 42
+
+def init_engine_es(dataset_file):
+	engine = SnipsNLUEngine(config=CONFIG_ES, random_state=seed)
+	with io.open(dataset_file) as f:
+		dataset = json.load(f)
+
+	engine.fit(dataset)
+
+	engine.persist("docbot/resources/engine")
+
+
+def parse(engine_dir):
+	engine = SnipsNLUEngine.from_path(engine_dir)
+	parsing = engine.parse("test phrase")
+	print(json.dumps(parsing, indent=2))
+
+
+
+def run():
+	if(len(sys.argv) == 3):
+		if(sys.argv[1] == "-t"):
+			# Train
+			dataset_file = abspath(sys.argv[2]) # dataset json file
+			init_engine_es(dataset_file)
+		elif(sys.argv[1] == "-p"):
+			# Parse
+			engine_dir = abspath(sys.argv[2]) # dataset json file
+			parse(engine_dir)
+
+		else:
+			print("Invalid argument", sys.argv[1])
+
+
+	else:
+		print("Invalid number of arguments")
